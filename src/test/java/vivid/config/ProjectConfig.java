4f4018d06a868bf9a5bffbea430e54633f4fa4c5
nelak <elakovnikita24@yandex.ru>
2022-05-22 13:46:50 +0300

    delete deafult value from config

--- src/test/java/vivid/config/ProjectConfig.java
+++ src/test/java/vivid/config/ProjectConfig.java
@@ -8,18 +8,15 @@ public interface ProjectConfig extends Config {
     String remoteDriverUrl();
 
     @Key("videoStorage")
-    @DefaultValue("")
     String videoStorage();
 
     @Key("baseurl")
     String webUrl();
 
     @Key("browser")
-    @DefaultValue("chrome")
     String browser();
 
     @Key("browser.size")
-    @DefaultValue("1920*1080")
     String browserSize();
 
     @Key("browser.version")
